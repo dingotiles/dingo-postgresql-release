603977a693c879a17c475e87fdef2552f55cd332
Dr Nic Williams <drnicwilliams@gmail.com>
2015-10-30 14:50:55 -0700

    scripts to find leader's connection info

---
+++ scripts/leader.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $DIR/..
+
+cf_service_id=$1
+etcd_cluster=${etcd_cluster:-10.244.4.2:4001}
+
+if [[ -z "${cf_service_id}" ]]; then
+  echo "USAGE: ./scripts/leader.sh <cf-service-uuid>"
+  exit 1
+fi
+
+internal_id=cf-${cf_service_id}
+
+leader_id=$(curl -s ${etcd_cluster}/v2/keys/service/${internal_id}/leader | jq -r .node.value)
+if [[ -z "${leader_id}" ]]; then
+  echo "Cluster ${internal_id} not found or leader not available yet"
+  exit 1
+fi
+
+curl -s ${etcd_cluster}/v2/keys/service/${internal_id}/members/${leader_id} | jq -r .node.value | jq -r '.conn_url' | \
+  xargs -L1 ./scripts/strip_uri.sh
---
+++ scripts/strip_uri.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+DATABASE_URL=${DATABASE_URL:-$1}
+# extract the protocol
+proto="`echo $DATABASE_URL | grep '://' | sed -e's,^\(.*://\).*,\1,g'`"
+# remove the protocol
+url=`echo $DATABASE_URL | sed -e s,$proto,,g`
+
+# extract the user and password (if any)
+userpass="`echo $url | grep @ | cut -d@ -f1`"
+pass=`echo $userpass | grep : | cut -d: -f2`
+if [ -n "$pass" ]; then
+    user=`echo $userpass | grep : | cut -d: -f1`
+else
+    user=$userpass
+fi
+
+# extract the host -- updated
+hostport=`echo $url | sed -e s,$userpass@,,g | cut -d/ -f1`
+port=`echo $hostport | grep : | cut -d: -f2`
+if [ -n "$port" ]; then
+    host=`echo $hostport | grep : | cut -d: -f1`
+else
+    host=$hostport
+fi
+
+# extract the path (if any)
+path="`echo $url | grep / | cut -d/ -f2-`"
+
+echo $proto $user $pass $host $port $path
