3ca8af3c5a72f477841ee46254a3551ca747a0bb
Dr Nic Williams <drnicwilliams@gmail.com>
2015-11-08 07:43:07 -0800

    include host ip in service_states.sh

--- scripts/service_states.sh
+++ scripts/service_states.sh
@@ -15,7 +15,16 @@ for servicekey in ${services[@]}; do
   printf "$(basename $servicekey) "
   members=$(curl -sL "${ETCD_CLUSTER}/v2/keys${servicekey}/members/?sorted=true" | jq -r ".node.nodes[].value")
   for member in ${members}; do
-    printf "$(echo $member | jq -r .role)/$(echo $member | jq -r .state) "
+    conn_url=$(echo $member | jq -r .conn_url)
+    # matching for "postgres://replicator:replicator@10.244.21.6:32780/postgres"
+    regexp="postgres:\/\/([^:]+):([^@]+)@([^:]+):([^\/]+)/([^\/]+)"
+    if [[ "${conn_url}" =~ $regexp ]]; then
+      host="${BASH_REMATCH[3]}"
+      port="${BASH_REMATCH[4]}"
+      printf "$(echo $member | jq -r .role)/$(echo $member | jq -r .state)/${host} "
+    else
+      printf "$(echo $member | jq -r .role)/$(echo $member | jq -r .state)/--- "
+    fi
   done
   echo
 done
