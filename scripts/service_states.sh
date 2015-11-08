c1506b7536feb18a04eb2635837c2b1195e6b045
Dr Nic Williams <drnicwilliams@gmail.com>
2015-11-08 07:59:52 -0800

    service_states shows if a cluster was deleted

--- scripts/service_states.sh
+++ scripts/service_states.sh
@@ -13,18 +13,23 @@ ETCD_CLUSTER=${ETCD_CLUSTER:-10.244.4.2:4001}
 services=$(curl -sL "${ETCD_CLUSTER}/v2/keys/service/?sorted=true" | jq -r ".node.nodes[].key")
 for servicekey in ${services[@]}; do
   printf "$(basename $servicekey) "
-  members=$(curl -sL "${ETCD_CLUSTER}/v2/keys${servicekey}/members/?sorted=true" | jq -r ".node.nodes[].value")
-  for member in ${members}; do
-    conn_url=$(echo $member | jq -r .conn_url)
-    # matching for "postgres://replicator:replicator@10.244.21.6:32780/postgres"
-    regexp="postgres:\/\/([^:]+):([^@]+)@([^:]+):([^\/]+)/([^\/]+)"
-    if [[ "${conn_url}" =~ $regexp ]]; then
-      host="${BASH_REMATCH[3]}"
-      port="${BASH_REMATCH[4]}"
-      printf "$(echo $member | jq -r .role)/$(echo $member | jq -r .state)/${host} "
-    else
-      printf "$(echo $member | jq -r .role)/$(echo $member | jq -r .state)/--- "
-    fi
-  done
-  echo
+  membersRaw=$(curl -sL "${ETCD_CLUSTER}/v2/keys${servicekey}/members/?sorted=true" | jq -r ".node.nodes" 2>&1)
+  if [[ "${membersRaw}" =~ "null" ]]; then
+    echo "deleted"
+  else
+    members=$(echo $membersRaw | jq -r ".[].value")
+    for member in ${members}; do
+      conn_url=$(echo $member | jq -r .conn_url)
+      # matching for "postgres://replicator:replicator@10.244.21.6:32780/postgres"
+      regexp="postgres:\/\/([^:]+):([^@]+)@([^:]+):([^\/]+)/([^\/]+)"
+      if [[ "${conn_url}" =~ $regexp ]]; then
+        host="${BASH_REMATCH[3]}"
+        port="${BASH_REMATCH[4]}"
+        printf "$(echo $member | jq -r .role)/$(echo $member | jq -r .state)/${host} "
+      else
+        printf "$(echo $member | jq -r .role)/$(echo $member | jq -r .state)/--- "
+      fi
+    done
+    echo
+  fi
 done
