4fc6f13f989bc7117220dfb1dc3547eef622421e
Dr Nic Williams <drnicwilliams@gmail.com>
2015-11-23 16:08:33 -0800

    create pg95 images that include pg_rewind

---
+++ images/postgresql95-patroni/scripts/postgres/start_pg.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e #fail fast
+
+export PG_VERSION=9.4
+
+export PATH=/usr/lib/postgresql/${PG_VERSION}/bin:$PATH
+DATA_DIR=/data
+WALE_ENV_DIR=${WALE_ENV_DIR:-${DATA_DIR}/wal-e/env}
+
+# pass thru environment variables into an env dir for postgres user's archive/restore commands
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $DIR
+
+echo "Starting backups..."
+envdir ${WALE_ENV_DIR} ${DIR}/regular_backup.sh
+
+echo "Starting Patroni..."
+cd /
+python /patroni.py /patroni/postgres.yml
