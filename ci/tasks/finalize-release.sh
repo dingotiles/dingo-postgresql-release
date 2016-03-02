23a7ecbc469997bdf86201b4eb2247cb421c1d7d
Dr Nic Williams <drnicwilliams@gmail.com>
2016-03-03 07:15:32 +1000

    port finalize-release from concourse's own ci

---
+++ ci/tasks/finalize-release.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+version=$(cat version/number)
+RELEASE_NAME=${RELEASE_NAME:-"patroni-docker"}
+
+FINAL_RELEASE_TARBALL=$PWD/final-release-tarball
+FINAL_RELEASE_REPO=$PWD/final-release-repo
+
+CANDIDATE_DIR=$PWD/candidate-release
+
+git clone boshrelease $FINAL_RELEASE_REPO
+cd $FINAL_RELEASE_REPO
+
+cat > config/private.yml << EOF
+---
+blobstore:
+  s3:
+    access_key_id: ${aws_access_key_id}
+    secret_access_key: ${aws_secret_access_key}
+EOF
+
+bosh -n create release --final -v ${version}
+
+if [[ -z "$(git config --global user.name)" ]]
+then
+  git config --global user.name "Concourse Bot"
+  git config --global user.email "drnic+bot@starkandwayne.com"
+fi
+
+RELEASE_YML=$PWD/releases/${RELEASE_NAME}/${RELEASE_NAME}-${VERSION}.yml
+RELEASE_TGZ=$PWD/releases/${RELEASE_NAME}/${RELEASE_NAME}-${VERSION}.tgz
+
+if [ -e ${RELEASE_YML} ]; then
+  echo "release already created; making tarball..."
+  bosh -n create release --with-tarball ${RELEASE_YML}
+else
+  echo "finalizing release"
+  bosh -n finalize release --version "$VERSION" ${CANDIDATE_DIR}/${RELEASE_NAME}-*.tgz
+  git add -A
+  git commit -m "release v${VERSION}"
+fi
+
+mv ${RELEASE_TGZ} ${FINAL_RELEASE_TARBALL}
