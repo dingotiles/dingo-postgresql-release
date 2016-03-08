661426029f6e7ed90f15194814eb0c17de725087
Dr Nic Williams <drnicwilliams@gmail.com>
2016-03-08 10:39:05 +1000

    testflight can deploy different images if desired; upstream or embedded

--- ci/tasks/create-manifest.sh
+++ ci/tasks/create-manifest.sh
@@ -33,9 +33,17 @@ properties:
     - tag="{{.Name}}"
 EOF
 
+cat > tmp/docker_image.yml <<EOF
+meta:
+  docker_image:
+    image: ${docker_image_image}
+    tag: ${docker_image_tag}
+EOF
+
 bosh target ${bosh_target}
 
 export DEPLOYMENT_NAME=${deployment_name}
-./templates/make_manifest warden embedded templates/services-solo.yml templates/jobs-etcd.yml tmp/syslog.yml
+./templates/make_manifest warden ${docker_image_source} templates/services-solo.yml \
+  templates/jobs-etcd.yml tmp/syslog.yml tmp/docker_image.yml
 
 cp tmp/${DEPLOYMENT_NAME}*.yml ${manifest_dir}/manifest.yml
