5ff8e115c85be18114c7492d4c56a207df1145f1
Dr Nic Williams <drnicwilliams@gmail.com>
2016-03-10 13:27:21 +1000

    psql.sh makes it easy to run psql against current tutorial leader

---
+++ images/tutorial/psql.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# USAGE:
+# Run pgbench against current master
+#   env $(cat tmp/tutorial.env| xargs) ./images/tutorial/psql.sh
+
+if [[ "${ETCD_CLUSTER}X" == "X" ]]; then
+  echo "Requires \$ETCD_CLUSTER"
+  exit 1
+fi
+PATRONI_SCOPE=${PATRONI_SCOPE:-my_first_cluster}
+
+leader=$(curl -s ${ETCD_CLUSTER}/v2/keys/service/${PATRONI_SCOPE}/leader | jq -r .node.value)
+if [[ "${leader}" == "null" ]]; then
+  echo "No current leader for cluster"
+  exit 1
+fi
+
+pg_uri=$(curl -s ${ETCD_CLUSTER}/v2/keys/service/${PATRONI_SCOPE}/members/${leader} | jq -r .node.value | jq -r .conn_url)
+if [[ "${pg_uri}X" == "X" || "${pg_uri}" == "null" ]]; then
+  echo "Leader ${leader} does not have any member .conn_url data"
+  exit 1
+fi
+
+if [[ "${1}X" == "X" ]]; then
+  psql ${pg_uri} -c "select * from pgbench_tellers;"
+else
+  psql ${pg_uri} $@
+fi
