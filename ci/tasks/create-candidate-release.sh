06fde0836a1b5994a1d2a4dbdbeb85ebd8458422
Justin Carter <jcarter@starkandwayne.com>
2016-03-11 20:02:30 +0100

    Intorduce edge image

--- ci/tasks/create-candidate-release.sh
+++ ci/tasks/create-candidate-release.sh
@@ -7,6 +7,13 @@ OUTPUT="$PWD/candidate-release"
 VERSION="$(cat version/number)"
 RELEASE_NAME=${RELEASE_NAME:-patroni-docker}
 
+export IMAGE_BASE_DIR=$(pwd)/images
+
 cd boshrelease
-bosh -n create release --with-tarball --name $RELEASE_NAME --version "$VERSION"
+
+rake images:cleanout
+rake images:package
+rake jobs:update_spec
+
+bosh -n create release --with-tarball --force --name $RELEASE_NAME --version "$VERSION"
 mv dev_releases/$RELEASE_NAME/$RELEASE_NAME-*.tgz "$OUTPUT"
