d52c6b3863cce2311dc6e5340b0e05f2d5f95d10
Justin Carter <jcarter@starkandwayne.com>
2016-03-14 15:05:19 +0100

    Don't embed images in candidate release

--- ci/tasks/create-candidate-release.sh
+++ ci/tasks/create-candidate-release.sh
@@ -11,9 +11,10 @@ export IMAGE_BASE_DIR=$(pwd)/images
 
 cd boshrelease
 
-rake images:cleanout
-rake images:package
-rake jobs:update_spec
+# add back when we want to include embedded images in testflight test
+# rake images:cleanout
+# rake images:package
+# rake jobs:update_spec
 
 bosh -n create release --with-tarball --force --name $RELEASE_NAME --version "$VERSION"
 mv dev_releases/$RELEASE_NAME/$RELEASE_NAME-*.tgz "$OUTPUT"
