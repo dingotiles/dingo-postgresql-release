db785bdd30f7c8432404ab977be3a70d3c02228c
Dr Nic Williams <drnicwilliams@gmail.com>
2016-03-16 10:45:45 +1000

    rename dependency releases ready for upload to github release

---
+++ ci/tasks/rename-dependency-releases.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e # fail fast
+set -x # print commands
+
+releases=(etcd remote-syslog)
+for release in "${releases[@]}"; do
+  version=$(cat $release/version)
+  mv $release/release.tgz dependency-releases/$release-$version.tgz
+done
