cf88b1aff3283acef74e012ac5bd0100cd472fd3
Justin Carter <jcarter@starkandwayne.com>
2016-03-17 15:09:23 +0100

    Fix default image in run script

--- images/postgresql95-patroni/scripts/run.sh
+++ images/postgresql95-patroni/scripts/run.sh
@@ -8,7 +8,7 @@ mkdir -p $DATA_DIR
 DOCKER_IP=$(hostname --ip-address)
 
 REGISTRATOR_PREFIX=${REGISTRATOR_PREFIX:-}
-REGISTRATOR_DOCKER_IMAGE=${REGISTRATOR_DOCKER_IMAGE:-postgres95-patroni} # used as path by registrator entries
+REGISTRATOR_DOCKER_IMAGE=${REGISTRATOR_DOCKER_IMAGE:-dingo-postgresql95} # used as path by registrator entries
 
 if [[ -z "${NAME}" ]]; then
   echo "Requires \$NAME to look up container in registrator"
