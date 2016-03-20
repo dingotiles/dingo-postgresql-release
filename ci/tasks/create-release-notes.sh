e6890f86dd1eb4e7f362ad2d0a7a73396654f5cc
Dr Nic Williams <drnicwilliams@gmail.com>
2016-03-20 14:29:09 +1000

    escape backticks so they aren't evaluated

--- ci/tasks/create-release-notes.sh
+++ ci/tasks/create-release-notes.sh
@@ -11,14 +11,14 @@ echo v${version} > $NOTES/release-name
 cat > $NOTES/notes.md <<EOF
 To upload BOSH releases:
 
-```
+\`\`\`
 curl -s "https://api.github.com/repos/dingotiles/patroni-docker-boshrelease/releases/tags/v${version}" | jq -r ".assets[].browser_download_url"  | grep tgz | \
   xargs -L1 bosh upload release --skip-if-exists
-```
+\`\`\`
 
 Or get URLs for BOSH releases:
 
-```
+\`\`\`
 curl -s "https://api.github.com/repos/dingotiles/patroni-docker-boshrelease/releases/tags/v${version}" | jq -r ".assets[].browser_download_url"  | grep tgz
-```
+\`\`\`
 EOF
