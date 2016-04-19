8fc5a6a485d0c75090e203444cc2a89e09bf98a2
Justin Carter <jcarter@starkandwayne.com>
2016-04-19 14:34:39 +0200

    Add comments back in

--- images/postgresql95-patroni/scripts/run.sh
+++ images/postgresql95-patroni/scripts/run.sh
@@ -134,9 +134,12 @@ postgresql:
   #   username: postgres
   #   password: starkandwayne
   pg_hba:
+  # Allow any user from any host to connect to database
+  # "postgres" if the user's password is correctly supplied.
+  # TYPE    DATABASE  USER    ADDRESS   METHOD
+  - host    replication  dvw7DJgqzFBJC8 0.0.0.0/0 md5
   - host    postgres  all     0.0.0.0/0 md5
   - hostssl postgres  all     0.0.0.0/0 md5
-  - host    replication  dvw7DJgqzFBJC8 0.0.0.0/0 md5
   replication: # replication username, user will be created during initialization
     username: dvw7DJgqzFBJC8
     password: jkT3TTNebfrh6C
