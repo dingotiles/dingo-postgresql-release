f1d1296549a49d72436977c740b2c63bb60ce0fd
Dr Nic Williams <drnicwilliams@gmail.com>
2016-04-22 13:39:25 +1000

    WIP script to find/delete orphaned service instsances that don't map to CF requests [ci skip]

---
+++ scripts/orphaned_service_instances.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# USAGE: watch -n1 "./scripts/service_instances.sh"
+
+set +e # fail fast
+set +x # print commands
+
+# global GUID for "cluster" service plan
+service_plan_guid=${service_plan_guid:-1545e30e-6dc3-11e5-826a-6c4008a663f0}
+ETCD_CLUSTER=${ETCD_CLUSTER:-$ETCD_HOST_PORT}
+if [[ -z $ETCD_CLUSTER ]]; then
+  echo "Requires \$ETCD_CLUSTER or \$ETCD_HOST_PORT"
+  exit 1
+fi
+
+cf_target=$(cat ~/.cf/config.json | jq -r .Target)
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $DIR/..
+
+# cf curl /v2/service_plans
+# cf curl /v2/service_plans/${service_plan_guid}
+if [[ "$(cf curl /v2/service_plans/${service_plan_guid}/service_instances | jq -r .error_code)" == "null" ]]; then
+  requested_service_instances=($(cf curl /v2/service_plans/${service_plan_guid}/service_instances | jq -r ".resources[].metadata.guid"))
+else
+  echo "Service plan ${service_plan_guid} is not registered on ${cf_target}"
+  cf target
+  exit 1
+fi
+
+running_service_instances=($(curl -s ${ETCD_CLUSTER}/v2/keys/service | jq -r ".node.nodes[].key" | sed -e 's%/service/%%'))
+
+for running in "${running_service_instances[@]}"; do
+  found_requested_service_instance=0
+  for requested in "${requested_service_instances[@]}"; do
+    if [[ $running == $requested ]]; then
+      found_requested_service_instance=1
+    fi
+  done
+  if [[ "$found_requested_service_instance" == "0" ]]; then
+    echo "Service instance ${running} not requested by ${cf_target} users"
+  fi
+done
