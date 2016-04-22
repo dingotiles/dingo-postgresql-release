a284c2e897280631a0471f7fcc64e06359e63ee3
Dr Nic Williams <drnicwilliams@gmail.com>
2016-04-22 14:05:35 +1000

    delete orphaned service instances via broker

--- scripts/orphaned_service_instances.sh
+++ scripts/orphaned_service_instances.sh
@@ -5,7 +5,12 @@
 set +e # fail fast
 set +x # print commands
 
-# global GUID for "cluster" service plan
+BROKER_URL=${BROKER_URL:-}
+BROKER_USERNAME=${BROKER_USERNAME:-}
+BROKER_PASSWORD=${BROKER_PASSWORD:-}
+
+# Global GUID for "cluster" service plan
+SERVICE_GUID=${SERVICE_GUID:-beb5973c-e1b2-11e5-a736-c7c0b526363d}
 SERVICE_PLAN_GUID=${SERVICE_PLAN_GUID:-1545e30e-6dc3-11e5-826a-6c4008a663f0}
 ETCD_CLUSTER=${ETCD_CLUSTER:-$ETCD_HOST_PORT}
 if [[ -z $ETCD_CLUSTER ]]; then
@@ -15,6 +20,19 @@ fi
 
 cf_target=$(cat ~/.cf/config.json | jq -r .Target)
 
+shown_missing_broker=0
+function delete_service_instance {
+  instance_guid=$1
+  if [[ "${BROKER_URL}X" == "X" ]]; then
+    if [[ $shown_missing_broker == 0 ]]; then
+      echo "Provide \$BROKER_URL, \$BROKER_USERNAME, and \$BROKER_PASSWORD to expunge orphans"
+      shown_missing_broker=1
+    fi
+  else
+    echo "Deleting orphan ${instance_guid} via dingo-postgresql service broker ${BROKER_URL}"
+    curl -v -X DELETE -u ${BROKER_USERNAME}:${BROKER_PASSWORD} "${BROKER_URL}/v2/service_instances/${instance_guid}?service_id=${SERVICE_GUID}&plan_id=${SERVICE_PLAN_GUID}"
+  fi
+}
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR/..
 
@@ -23,7 +41,7 @@ if [[ "$(cf curl /v2/service_plans/${SERVICE_PLAN_GUID}/service_instances | jq -
 else
   echo "Service plan ${SERVICE_PLAN_GUID} is not registered on ${cf_target}"
   cf target
-  exit 1
+  # exit 1
 fi
 
 running_service_instances=($(curl -s ${ETCD_CLUSTER}/v2/keys/service | jq -r ".node.nodes[].key" | sed -e 's%/service/%%'))
@@ -37,5 +55,6 @@ for running in "${running_service_instances[@]}"; do
   done
   if [[ "$found_requested_service_instance" == "0" ]]; then
     echo "Service instance ${running} not requested by ${cf_target} users"
+    delete_service_instance ${running}
   fi
 done
