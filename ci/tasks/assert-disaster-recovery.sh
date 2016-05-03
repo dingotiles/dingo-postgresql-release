28908549fd493cbd409cc09cfeada3e6f0867b82
Justin Carter <jcarter@starkandwayne.com>
2016-05-03 21:33:32 +0200

    Check multiple times in dr

--- ci/tasks/assert-disaster-recovery.sh
+++ ci/tasks/assert-disaster-recovery.sh
@@ -15,7 +15,23 @@ cf login --skip-ssl-validation \
 cf service-key dr-test dr-test-binding
 pg_uri=$(cf service-key dr-test dr-test-binding | grep '"uri"' | grep -o 'postgres://.*/postgres' | sed "s/@.*:/@${broker_ip}:/")
 
-psql ${pg_uri} -c 'SELECT * FROM disasterrecoverytest;' | grep 'dr-test'
+set +x
+for ((n=0;n<60;n++)); do
+    found='false'
+    set -x
+    table=$(psql ${pg_uri} -c 'SELECT * FROM disasterrecoverytest;')
+    set +x
+    if echo ${table} | grep 'dr-test'
+        found='true'
+        break
+    fi
+    sleep 1
+done
+set -x
+
+if [[ ${found} != 'true' ]]; then
+    exit 1
+fi
 
 cf delete-service-key -f dr-test dr-test-binding
 cf delete-service -f dr-test
