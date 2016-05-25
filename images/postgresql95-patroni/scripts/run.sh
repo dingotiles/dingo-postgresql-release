81c5c82a5338692fbbcecb0be39fc24ef96ece3c
Justin Carter <jcarter@starkandwayne.com>
2016-05-24 20:21:42 -0700

    Remove comment [ci skip]

--- images/postgresql95-patroni/scripts/run.sh
+++ images/postgresql95-patroni/scripts/run.sh
@@ -118,7 +118,6 @@ indent_startup() {
   fi
 
 
-  # TODO secure the passwords!
   # TODO add host ip into postgresql.name to ensure unique if two containers have same local DOCKER_IP
 
   cat > /patroni/postgres.yml <<EOF
