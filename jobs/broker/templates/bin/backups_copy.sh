2e8948d927ce7932f6c499fc9b81ddca0004cec1
Dr Nic Williams <drnicwilliams@gmail.com>
2016-08-03 07:57:56 +1000

    backups_copy.sh is callback for requesting copy of wal/base_backups to new folder

---
+++ jobs/broker/templates/bin/backups_copy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+json=$(cat -)
+from_uri=$(echo $json | jq -r ".from_uri")
+to_uri=$(echo $json | jq -r ".to_uri")
+
+<% if_p("backups.database_storage.aws_access_key_id", "backups.database_storage.aws_secret_access_key", "backups.database_storage.region") do |aws_access_key_id, aws_secret_access_key, region| %>
+# setup awscli configuration
+mkdir -p ~/.aws
+chmod 700 ~/.aws
+
+cat > ~/.aws/credentials <<EOF
+[default]
+aws_access_key_id = <%= aws_access_key_id %>
+aws_secret_access_key = <%= aws_secret_access_key %>
+EOF
+chmod 600 ~/.aws/credentials
+
+cat > ~/.aws/config <<EOF
+[default]
+region = <%= region %>
+EOF
+chmod 600 ~/.aws/config
+
+aws s3 cp $from_uri $to_uri --recursive
+
+<% end.else do %>
+echo "No configuration provided to copy Amazon S3 folders"
+exit 1
+<% end %>
