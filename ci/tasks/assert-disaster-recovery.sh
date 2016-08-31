4027eaa10450c3a7dd3a3e817540380b737d7c4c
Dr Nic Williams <drnicwilliams@gmail.com>
2016-08-31 12:23:26 +1000

    integration tests run on diff bosh now

--- ci/tasks/assert-disaster-recovery.sh
+++ ci/tasks/assert-disaster-recovery.sh
@@ -13,7 +13,7 @@ cf auth admin admin
 cf t -o dr-test -s dr-test
 
 cf service-key dr-test dr-test-binding
-pg_uri=$(cf service-key dr-test dr-test-binding | grep '"uri"' | grep -o 'postgres://.*/postgres' | sed "s/@.*:/@${broker_ip}:/")
+pg_uri=$(cf service-key dr-test dr-test-binding | grep '"uri"' | grep -o 'postgres://.*/postgres' | sed "s/@.*:/@${router_ip}:/")
 
 set +x
 wait_for_database_recovery $pg_uri
